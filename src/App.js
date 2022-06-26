c74439206280eaab625111224fe6868942e159b5
Stefano L <stefanosemail@protonmail.com>
2022-06-26 22:49:00 +0100

    comps update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from "react-dom";
-import Pet from "./components/Pet";
+import SearchParams from "./components/SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <Pet name={"luna"} animal={"dog"} breed={"chilli"} />
-      <Pet name={"peppa"} animal={"fish"} breed={"tank"} />
-      <Pet name={"donkey"} animal={"cat"} breed={"mix"} />
+      <h1>Pets R Us</h1>
+      <SearchParams />
     </div>
   );
 };
