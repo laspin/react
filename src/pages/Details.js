a6266f0a1e68a419e1d5218f5beeba352f1cebb3
Stefano L <stefanosemail@protonmail.com>
2022-06-28 22:57:04 +0100

    details carousel

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -7,6 +8,7 @@ const Details = () => {
   const { id } = useParams();
   const [details, setDetails] = useState({});
   const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState(0);
 
   useEffect(() => {
     const doFetchDetails = async () => {
@@ -19,21 +21,50 @@ const Details = () => {
     return () => doFetchDetails;
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const handleImageClick = event => setActive(+event.target.dataset.index); //typeof number
+
   return (
     <>
       {loading ? (
         <h2 id="loading">Loading...</h2>
       ) : (
-        <div className="details">
-          <div className="details-info">
-            <h1>{details.name}</h1>
-            <h2>
-              {details.animal} — {details.breed} — {details.city}
-            </h2>
-            <button>Adopt {details.name}</button>
-            <p>{details.description}</p>
+        <>
+          <div className="details">
+            <div className="details-info">
+              <h1>{details.name}</h1>
+              <h2>
+                {details.animal} — {details.breed} — {details.city}
+              </h2>
+              <button>Adopt {details.name}</button>
+              <p>{details.description}</p>
+            </div>
           </div>
-        </div>
+
+          <div className="carousel">
+            <img
+              src={details.images[active]}
+              alt="animal"
+            />
+            <div className="carousel-smaller">
+              {details?.images.map((photo, index) => (
+                // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
+                <figure
+                  className="image-container"
+                  onClick={handleImageClick}
+                  key={index}
+                >
+                  <img
+                    key={index}
+                    src={photo}
+                    data-index={index}
+                    className={index === active ? 'active' : ''}
+                    alt={photo}
+                  />
+                </figure>
+              ))}
+            </div>
+          </div>
+        </>
       )}
     </>
   );
