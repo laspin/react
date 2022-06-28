28c32d2cadd57909f51ecae96e9a2b667c8069cf
Stefano L <stefanosemail@protonmail.com>
2022-06-28 19:35:14 +0100

    update fetched details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,8 +1,42 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+const URL = 'http://pets-v2.dev-apis.com/pets?id=';
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>Hello number {id}</h2>;
+  const [details, setDetails] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const doFetchDetails = async () => {
+      let res = await fetch(`${URL}${id}`);
+      let data = await res.json();
+      setDetails(data.pets[0]);
+      setLoading(false);
+    };
+    doFetchDetails();
+    return () => doFetchDetails;
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {loading ? (
+        <h2 id="loading">Loading...</h2>
+      ) : (
+        <div className="details">
+          <div className="details-info">
+            <h1>{details.name}</h1>
+            <h2>
+              {details.animal} — {details.breed} — {details.city}
+            </h2>
+            <button>Adopt {details.name}</button>
+            <p>{details.description}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Details;
